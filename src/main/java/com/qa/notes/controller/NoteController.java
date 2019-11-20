76a5713973e2ee704d1efee76020ef8510867a16
user <you@example.com>
2019-11-20 09:46:57 +0000

    Fail

--- src/main/java/com/qa/notes/controller/NoteController.java
+++ src/main/java/com/qa/notes/controller/NoteController.java
@@ -27,15 +27,14 @@ import com.qa.notes.service.NoteService;
 @RestController
 @CrossOrigin("*")
 public class NoteController {
-	
-	@Autowired
+		@Autowired
 	private NoteService service;
 
 	@RequestMapping(path = "note/", method = {RequestMethod.GET})
 	@ResponseStatus(code = HttpStatus.OK)
 	public List<NoteDto> getNotes(){
 		return new ArrayList<NoteDto>(service.getNotes());
-	}
+	
 	
 	@RequestMapping(path = "note/", method = {RequestMethod.POST})
 	@ResponseStatus(code = HttpStatus.CREATED)
